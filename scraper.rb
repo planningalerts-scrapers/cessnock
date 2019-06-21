20e3de7da5fda08a7acf4888a89dcf41c3f0c86c
Matthew Landauer <mlandauer@gmail.com>
2019-06-21 13:59:49 +1000

    Update to latest gem

--- scraper.rb
+++ scraper.rb
@@ -1,7 +1,3 @@
 require "masterview_scraper"
 
-MasterviewScraper.scrape_and_save_period(
-  url: "http://datracker.cessnock.nsw.gov.au",
-  period: :last10days,
-  use_api: true
-)
+MasterviewScraper.scrape_and_save(:cessnock)
